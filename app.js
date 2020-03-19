b61d7f633a89e124c28faa4cc2876151d0189ff8
vladislav_lee <vladislav.lee.bekey@gmail.com>
2020-03-19 11:31:58 +0200

    fixed keys

--- app.js
+++ app.js
@@ -41,9 +41,7 @@ if (process.env.NODE_ENV === 'production') {
 
     app.get('*', (req, res) => {
         res.sendFile(
-            path.resolve(
-                __dirname, 'client', 'dist', 'client', 'index.html'
-            )
+            path.resolve(__dirname, 'client', 'dist', 'client', 'index.html')
         )
     })
 }
